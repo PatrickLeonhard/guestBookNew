d4a4364b03c48aa78cf324384d3294d814ef6178
Patrick Leonhard <patrick.leonhard3@gmail.com>
2025-01-28 15:26:46 +0100

    3. Commit before config the test-enviornment Chapter 17

--- tests/SpamCheckerTest.php
+++ tests/SpamCheckerTest.php
@@ -11,41 +11,46 @@ use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SpamCheckerTest extends TestCase
 {
-   public function testSpamScoreWithInvalidRequest(): void
-   {
-       $comment = new Comment();
-       $comment->setCreatedAtValue();
-       $context = [];
+    public function testSpamScoreWithInvalidRequest(): void
+    {
+        $comment = new Comment();
+        $comment->setCreatedAtValue();
+        $context = [];
 
-       $client = new MockHttpClient([new MockResponse('invalid', ['response_headers' => ['x-akismet-debug-help: Invalid key']])]);
-       $checker = new SpamChecker($client, 'abcde');
+        $client = new MockHttpClient([new MockResponse('invalid', ['response_headers' => ['x-akismet-debug-help: Invalid key']])]);
+        $checker = new SpamChecker($client, 'abcde');
 
-       $this->expectException(\RuntimeException::class);
-       $this->expectExceptionMessage('Unable to check for spam: invalid (Invalid key).');
-       $checker->getSpamScore($comment, $context);
-   }
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to check for spam: invalid (Invalid key).');
+        $checker->getSpamScore($comment, $context);
+    }
 
-   public function testSpamScore(int $expectedScore, ResponseInterface $response, Comment $comment, array $context): void
-   {
-       $client = new MockHttpClient([$response]);
-       $checker = new SpamChecker($client, 'abcde');
+    /**
+     * @dataProvider provideComments
+     */
+    public function testSpamScore(int $expectedScore, ResponseInterface $response, Comment $comment, array $context)
+    {
+        $client = new MockHttpClient([$response]);
+        $checker = new SpamChecker($client, 'abcde');
 
-       $score = $checker->getSpamScore($comment, $context);
-       $this->assertSame($expectedScore, $score);
-   }
-   public static function provideComments(): iterable
-   {
-       $comment = new Comment();
-       $comment->setCreatedAtValue();
-       $context = [];
+        $score = $checker->getSpamScore($comment, $context);
+        $this->assertSame($expectedScore, $score);
+    }
 
-       $response = new MockResponse('', ['response_headers' => ['x-akismet-pro-tip: discard']]);
-       yield 'blatant_spam' => [2, $response, $comment, $context];
 
-       $response = new MockResponse('ture');
-       yield 'spam' => [1, $response, $comment, $context];
+    public static function provideComments(): iterable
+    {
+        $comment = new Comment();
+        $comment->setCreatedAtValue();
+        $context = [];
 
-       $response = new MockResponse('false');
-       yield 'ham' => [0, $response, $comment, $context];
-   }
+        $response = new MockResponse('', ['response_headers' => ['x-akismet-pro-tip: discard']]);
+        yield 'blatant_spam' => [2, $response, $comment, $context];
+
+        $response = new MockResponse('true');
+        yield 'spam' => [1, $response, $comment, $context];
+
+        $response = new MockResponse('false');
+        yield 'ham' => [0, $response, $comment, $context];
+    }
 }
