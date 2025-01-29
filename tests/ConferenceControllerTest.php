76d1114662a18451bfd195d73857c2b6ca1b5705
Patrick Leonhard <patrick.leonhard3@gmail.com>
2025-01-29 09:01:58 +0100

    5. Commit Chapter 18 Done

--- tests/ConferenceControllerTest.php
+++ tests/ConferenceControllerTest.php
@@ -8,10 +8,22 @@ class ConferenceControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $client = static::createPantherClient(['external_base_uri' => rtrim($_SERVER['SYMFONY_PROJECT_DEFAULT_ROUTE_URL'], '/')]);
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(1, $crawler->filter('h2'));
+        $client->clickLink('View');
+
+        $this->assertPageTitleContains('Paris');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Paris');
     }
     public function testConferencePage()
     {
